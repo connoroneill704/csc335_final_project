f7c9951b154af2d20af3830a69684ec25f74fd1e
AJBecerra <ajbecerra@arizona.edu>
2024-11-21 00:17:26 -0700

    added comments every function in game2048.java and removed 2048game.java since its the same file
    revamped addNewTile() in game2048.java to be more readable and efficient.
    
    added an exit input command to the 2048game.
    
    simple bug fixes to GUI2048.java
    
    changed visibility modifiers to the move functions
    
    completed functions to display the game in the GUI

---
+++ GUI2048.java
@@ -0,0 +1,137 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Random;
+public class GUI2048 extends JFrame{
+    private game2048 game;
+    private JLabel[][] tiles;
+    private JLabel scoreLabel;
+    private JPanel boardPanel;
+    private JPanel scorePanel;
+
+    public GUI2048(){
+        game = new game2048();
+    }
+
+    public static void main(String[] args) {
+        GUI2048 game = new GUI2048();
+        game.UI();
+    }
+
+    public void UI(){
+        JFrame frame = new JFrame();
+        frame.setTitle("2048 game");
+        frame.setSize(400,500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setLayout(new BorderLayout());
+        game = new game2048();
+
+        makeBoardPanel();
+        makeScorePanel();
+        frame.add(scoreLabel, BorderLayout.NORTH);
+        frame.add(boardPanel, BorderLayout.CENTER);
+        frame.setFocusable(true);
+        frame.requestFocusInWindow();
+
+        frame.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                useInput(e);
+            }
+        });
+        updateBoard();
+        frame.setVisible(true);
+    }
+    private void makeBoardPanel() {
+        boardPanel = new JPanel();
+        boardPanel.setLayout(new GridLayout(4, 4));
+        tiles = new JLabel[4][4];
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                tiles[i][j] = new JLabel();
+                tiles[i][j].setPreferredSize(new Dimension(100, 100));
+                tiles[i][j].setHorizontalAlignment(SwingConstants.CENTER);
+                tiles[i][j].setOpaque(true);
+                tiles[i][j].setBackground(Color.LIGHT_GRAY);
+                tiles[i][j].setBorder(BorderFactory.createLineBorder(Color.BLACK));
+                boardPanel.add(tiles[i][j]);
+            }
+        }
+    }
+    private void makeScorePanel(){
+        scorePanel = new JPanel();
+        scorePanel.setLayout(new GridLayout(1, 1));
+        scoreLabel = new JLabel("Score: " + game.getScore());
+        scoreLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        scorePanel.add(scoreLabel);
+
+    }
+    private void useInput(KeyEvent e){
+        switch (e.getKeyCode()){
+            case KeyEvent.VK_W:
+                game.moveUp();
+                break;
+            case KeyEvent.VK_A:
+                game.moveLeft();
+                break;
+            case KeyEvent.VK_S:
+                game.moveDown();
+                break;  
+            case KeyEvent.VK_D:
+                game.moveRight();
+                break;
+        }
+        game.addNewTile();
+        if (game.hasMoves() == false){
+            JOptionPane.showMessageDialog(null, "Game Over! Your score is: " + game.getScore());
+            System.exit(0);
+        }
+        updateBoard();
+        scoreLabel.setText("Score: " + game.getScore());
+
+    }
+    private void updateBoard(){
+        int[][] board = game.getBoard();
+        for (int i = 0; i < 4; i++){
+            for (int j = 0; j < 4; j++){
+                if (board[i][j] == 0){
+                    tiles[i][j].setText("");
+                    tiles[i][j].setBackground(Color.LIGHT_GRAY);
+                } else {
+                    tiles[i][j].setText("" + board[i][j]);
+                    tiles[i][j].setBackground(getNumberColor(board[i][j]));
+                }
+            }
+        }
+
+    }
+    private Color getNumberColor(int val){
+        switch (val){
+            case 2:
+                return new Color(160, 220, 250);
+            case 4:
+                return new Color(140, 220, 250);
+            case 8:
+                return new Color(120, 220, 250);
+            case 16:
+                return new Color(100, 220, 250);
+            case 32:
+                return new Color(80, 220, 250);
+            case 64:
+                return new Color(60, 220, 250);
+            case 128:
+                return new Color(40, 220, 250);
+            case 256:
+                return new Color(20, 220, 250);
+            case 512:
+                return new Color(0, 220, 250);
+            case 1024:
+                return new Color(20, 100, 250);
+            case 2048:
+                return new Color(10, 80, 225);
+            default:
+                return new Color(0, 60, 225);
+        }
+    }
+}
--- game2048.java
+++ game2048.java
@@ -1,8 +1,4 @@
-import java.util.Random;
-import java.util.Scanner;
-//For Time Trial
-import java.util.Timer;
-import java.util.TimerTask;
+import java.util.*;
 //For Sounds
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
@@ -11,7 +7,7 @@ import javax.sound.sampled.AudioInputStream;
 import java.io.File;
 
 
-public class game2048 {
+class game2048 {
     private int grid_size=4;
     private int[][] board;
     private int score;
@@ -25,35 +21,40 @@ public class game2048 {
     private Clip background;
 
     public game2048() {
-        board=new int[grid_size][grid_size];
+        board=new int[grid_size][grid_size];    // create size of board
         score=0;
-        addNewTile();
+        addNewTile();   // add 2 tiles to start game
         addNewTile();
     }
+    // add new tile to board
+    public void addNewTile() { 
+        Random rand=new Random();   // init random
+        int value=rand.nextDouble() < 0.9 ? 2 : 4;  // 90% chance of 2, 10% chance of 4
+        
+        List<int[]> emptyCells=new ArrayList<>();  // list for empty cells
 
-    private void addNewTile() {
-        Random rand=new Random();
-        int value=rand.nextDouble() < 0.9 ? 2 : 4;
-        int emptyCells=0;
-        for (int[] row : board)
-            for (int cell : row)
-                if (cell == 0) emptyCells++;
-        if (emptyCells == 0) return;
-        int location=rand.nextInt(emptyCells);
-        int count=0;
-        outerloop:
-        for (int i=0; i < grid_size; i++) {
-            for (int j=0; j < grid_size; j++) {
-                if (board[i][j] == 0) {
-                    if (count == location) {
-                        board[i][j]=value;
-                        break outerloop;
-                    }
-                    count++;
+        for (int i=0; i < grid_size; i++) { // loop rows
+            for (int j=0; j < grid_size; j++) { // loop columns
+                if (board[i][j] == 0) { // if cell is empty
+                    emptyCells.add(new int[]{i, j}); // add cell to list
                 }
             }
         }
+
+        if (emptyCells.isEmpty()) { // no empty cells
+            return; // skip add
+        }
+
+        int[] randCell=emptyCells.get(rand.nextInt(emptyCells.size())); // get random empty cell from the list
+        board[randCell[0]][randCell[1]]=value; // add new cell to board
+
     }
+
+    public int[][] getBoard() {
+        return board;
+    }
+
+    // display board
     private void printBoard() {
     	System.out.println("Score: " + score);
     	
@@ -63,7 +64,6 @@ public class game2048 {
     	if (mode.equals("Time Trial")) {
     		System.out.println("Time Left: " + timeLeft);
     	}
-        
         for (int i=0; i < grid_size; i++) {
             System.out.println("+-----".repeat(grid_size) + "+");
             for (int j=0; j < grid_size; j++) {
@@ -75,6 +75,7 @@ public class game2048 {
         System.out.println("+-----".repeat(grid_size) + "+\n");
     }
 
+    // cast moves
     private void move(char command) {
         made_move=false;
         if(command=='w'){
@@ -101,131 +102,136 @@ public class game2048 {
         }
     }
 
-    private void moveLeft() {
-        for (int i=0; i < grid_size; i++) {
-            int[] row=new int[grid_size];
-            int location=0;
-            boolean[] move_completed=new boolean[grid_size];
+    // move left
+    public void moveLeft() {
+        for (int i=0; i < grid_size; i++) { // loop rows
+            int[] row=new int[grid_size];   // create new row
+            int location=0; 
+            boolean[] move_completed=new boolean[grid_size];    // track moves
 
-            for (int j=0; j < grid_size; j++) {
-                if (board[i][j] != 0) {
-                    if (location > 0 && row[location - 1] == board[i][j] && !move_completed[location - 1]) {
-                        row[location - 1] *= 2;
-                        score += row[location - 1];
+            for (int j=0; j < grid_size; j++) { // loop columns
+                if (board[i][j] != 0) { // if cell is not empty
+                    if (location > 0 && row[location - 1] == board[i][j] && !move_completed[location - 1]) {    // if cell is same as cell to the left
+                        row[location - 1] *= 2;   // double cell value
+                        score += row[location - 1];   // update score
                         move_completed[location - 1]=true;
-                        if (row[location - 1] == 2048) won=true;
-                        made_move=true;
-                    } else {
-                        if (board[i][j] != row[location]) {
+                        if (row[location - 1] == 2048) won=true;    // if new cell is 2048, game is won
+                        made_move=true; 
+                    } else {    // if cell is different
+                        if (board[i][j] != row[location]) { 
                             made_move=true;
                         }
-                        row[location]=board[i][j];
-                        location++;
+                        row[location]=board[i][j];  // move cell to the left
+                        location++; // next cell
                     }
                 }
             }
-            board[i]=row;
+            board[i]=row;   // update row
         }
     }
 
-    private void moveRight() {
-        for (int i=0; i < grid_size; i++) {
-            int[] row=new int[grid_size];
-            int location=grid_size - 1;
-            boolean[] move_completed=new boolean[grid_size];
+    // move right
+    public void moveRight() {
+        for (int i=0; i < grid_size; i++) { // loop rows
+            int[] row=new int[grid_size];   // create new row
+            int location=grid_size - 1; 
+            boolean[] move_completed=new boolean[grid_size];    
 
-            for (int j=grid_size - 1; j >= 0; j--) {
-                if (board[i][j] != 0) {
-                    if (location < grid_size - 1 && row[location + 1] == board[i][j] && !move_completed[location + 1]) {
-                        row[location + 1] *= 2;
-                        score += row[location + 1];
-                        move_completed[location + 1]=true;
-                        if (row[location + 1] == 2048) won=true;
-                        made_move=true;
-                    } else {
+            for (int j=grid_size - 1; j >= 0; j--) {    // loop columns
+                if (board[i][j] != 0) { // if cell is not empty
+                    if (location < grid_size - 1 && row[location + 1] == board[i][j] && !move_completed[location + 1]) {    // if cell is same as cell to the right
+                        row[location + 1] *= 2;     // double cell value
+                        score += row[location + 1]; // update score
+                        move_completed[location + 1]=true;  
+                        if (row[location + 1] == 2048) won=true;    // if new cell is 2048, game is won
+                        made_move=true; 
+                    } else {    // if cell is different
                         if (board[i][j] != row[location]) {
                             made_move=true;
                         }
-                        row[location]=board[i][j];
-                        location--;
+                        row[location]=board[i][j];  // move cell to the right
+                        location--; // next cell
                     }
                 }
             }
-            board[i]=row;
+            board[i]=row;   // update row
         }
     }
 
-    private void moveUp() {
-        for (int j=0; j < grid_size; j++) {
-            int[] col=new int[grid_size];
-            int location=0;
+    // move up
+    public void moveUp() {
+        for (int j=0; j < grid_size; j++) { // loop columns
+            int[] col=new int[grid_size];   // create new column
+            int location=0; // start at first cell
             boolean[] move_completed=new boolean[grid_size];
 
-            for (int i=0; i < grid_size; i++) {
-                if (board[i][j] != 0) {
-                    if (location > 0 && col[location - 1] == board[i][j] && !move_completed[location - 1]) {
-                        col[location - 1] *= 2;
-                        score += col[location - 1];
-                        move_completed[location - 1]=true;
-                        if (col[location - 1] == 2048) won=true;
+            for (int i=0; i < grid_size; i++) { // loop rows
+                if (board[i][j] != 0) { // if cell is not empty
+                    if (location > 0 && col[location - 1] == board[i][j] && !move_completed[location - 1]) {    // if cell is same as cell above
+                        col[location - 1] *= 2;    // double cell value
+                        score += col[location - 1];   // update score
+                        move_completed[location - 1]=true;  
+                        if (col[location - 1] == 2048) won=true;    // if new cell is 2048, game is won
                         made_move=true;
-                    } else {
-                        if (board[i][j] != col[location]) {
+                    } else {    // if cell is different
+                        if (board[i][j] != col[location]) { 
                             made_move=true;
                         }
-                        col[location]=board[i][j];
-                        location++;
+                        col[location]=board[i][j];  // move cell up
+                        location++; // next cell
                     }
                 }
             }
-            for (int i=0; i < grid_size; i++) {
+            for (int i=0; i < grid_size; i++) { // update column
                 board[i][j]=col[i];
             }
         }
     }
 
-    private void moveDown() {
-        for (int j=0; j < grid_size; j++) {
-            int[] col=new int[grid_size];
-            int location=grid_size - 1;
+    // move down
+    public void moveDown() {
+        for (int j=0; j < grid_size; j++) { // loop columns
+            int[] col=new int[grid_size];   
+            int location=grid_size - 1; 
             boolean[] move_completed=new boolean[grid_size];
-            for (int i=grid_size - 1; i >= 0; i--) {
-                if (board[i][j] != 0) {
-                    if (location < grid_size - 1 && col[location + 1] == board[i][j] && !move_completed[location + 1]) {
-                        col[location + 1] *= 2;
-                        score += col[location + 1];
-                        move_completed[location + 1]=true;
+            for (int i=grid_size - 1; i >= 0; i--) {    // loop rows
+                if (board[i][j] != 0) { // if cell is not empty
+                    if (location < grid_size - 1 && col[location + 1] == board[i][j] && !move_completed[location + 1]) {    // if cell is same as cell below
+                        col[location + 1] *= 2;   // double cell value
+                        score += col[location + 1]; // update score
+                        move_completed[location + 1]=true;  
                         if (col[location + 1] == 2048) won=true;
-                        made_move=true;
-                    } else {
+                        made_move=true; 
+                    } else {    // if cell is different
                         if (board[i][j] != col[location]) {
                             made_move=true;
                         }
-                        col[location]=board[i][j];
-                        location--;
+                        col[location]=board[i][j];  // move cell down
+                        location--; // next cell
                     }
                 }
             }
-            for (int i=0; i < grid_size; i++) {
+            for (int i=0; i < grid_size; i++) { // update column
                 board[i][j]=col[i];
             }
         }
     }
-
-    private boolean hasMoves() {
-        for (int i=0; i < grid_size; i++) {
-            for (int j=0; j < grid_size; j++) {
-                if (board[i][j] == 0)
-                    return true;
-                if (j < grid_size - 1 && board[i][j] == board[i][j + 1])
-                    return true;
-                if (i < grid_size - 1 && board[i][j] == board[i + 1][j])
-                    return true;
+    // check if there are any moves left
+    public boolean hasMoves() {    
+        for (int i=0; i < grid_size; i++) { // loop rows
+            for (int j=0; j < grid_size; j++) { // loop columns
+                if (board[i][j] == 0)   // if cell is empty
+                    return true;    // moves left
+                if (j < grid_size - 1 && board[i][j] == board[i][j + 1])    // if cell is same as cell to the right
+                    return true;    // moves left
+                if (i < grid_size - 1 && board[i][j] == board[i + 1][j])    // if cell is same as cell below
+                    return true;    // moves left
             }
         }
-        return false;
+        return false;   // no moves left
     }
 
+    // game loop
     private void gameLoop() {
         Scanner scanner=new Scanner(System.in);
         //Check game modes
@@ -249,11 +255,15 @@ public class game2048 {
         		break;
         	}
         	//User input for move
-            System.out.print("Enter move (W/A/S/D): ");
+            System.out.print("Enter move (W/A/S/D/exit): ");
             String input=scanner.nextLine().toLowerCase();
             if (input.isEmpty()) {
-            	System.out.println("No input detected. Please use W, A, S, or D.");
+            	System.out.println("No input detected. Please use W, A, S, D, or exit.");
                 continue;
+            }
+            if (input.equals("exit")) {
+            	System.out.println("Quit");
+            	break;
             }
             char move=input.charAt(0);
             move(move);
@@ -276,6 +286,7 @@ public class game2048 {
         scanner.close();
     }
     
+    // Timer for Time Trial mode
     private void startTimer(int seconds) {
     	Timer timer = new Timer();
     	timeLeft = seconds;
@@ -294,6 +305,7 @@ public class game2048 {
     	timer.scheduleAtFixedRate(task, 0, 1000);
     }
     
+    // music
     private void playBackground() {
     	try {
     		File file = new File("Sounds/Cool.wav");
@@ -309,7 +321,8 @@ public class game2048 {
             System.err.println("Error playing background music: " + e.getMessage());
         }
     }
-    
+
+    // end music
     private void stopBackground() {
         if (background != null && background.isRunning()) {
         	background.stop();
@@ -317,6 +330,7 @@ public class game2048 {
         }
     }
     
+    // sound effects
     private void playSound(String soundFile) {
         try {
             File file = new File(soundFile);
@@ -357,6 +371,10 @@ public class game2048 {
         game.stopBackground();
         
         scanner.close();
+    }
+
+    public int getScore() {
+        return score;
     }
 }
 
