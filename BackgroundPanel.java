7e498c6b1bd66847597c11ca65d4961f13764212
connoroneill704 <150087351+connoroneill704@users.noreply.github.com>
2024-12-06 11:41:34 -0700

    Add files via upload

---
+++ BackgroundPanel.java
@@ -0,0 +1,24 @@
+import java.awt.Graphics;
+import java.awt.Image;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+
+class BackgroundPanel extends JPanel {
+    private Image backgroundImage;
+
+    public BackgroundPanel(String imagePath) {
+        try {
+            backgroundImage = new ImageIcon(imagePath).getImage();
+        } catch (Exception e) {
+            System.err.println("Error loading background image: " + e.getMessage());
+        }
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        if (backgroundImage != null) {
+            g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this);
+        }
+    }
+}
